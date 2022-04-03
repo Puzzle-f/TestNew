1d609a5432733615f337925fe50dc2c2dfcfeed9
[Puzzle] <[puzzleuri@mail.ru]>
2022-04-03 19:51:53 +0300

    доработана DetailActivity

--- app/src/main/java/com/geekbrains/tests/view/search/ViewContract.kt
+++ app/src/main/java/com/geekbrains/tests/view/search/ViewContract.kt
@@ -2,13 +2,4 @@ package com.geekbrains.tests.view
 
 import com.geekbrains.tests.model.SearchResult
 
-interface ViewContract {
-    fun displaySearchResults(
-        searchResults: List<SearchResult>,
-        totalCount: Int
-    )
-
-    fun displayError()
-    fun displayError(error: String)
-    fun displayLoading(show: Boolean)
-}
+interface ViewContract
